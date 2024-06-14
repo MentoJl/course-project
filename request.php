4fcc3a01dcf56fea95bde51f2be2f9920379026b
TAMOTO24 <sanicyll@gmail.com>
2024-06-15 01:07:08 +0300

    update

--- request.php
+++ request.php
@@ -1,5 +1,8 @@
 <?php
 require 'slim-skeleton/vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -9,10 +12,6 @@ $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-// $app->options('/{routes:.+}', function ($request, $response, $args) {
-//     return $response;
-// });
 
 function sortBy($link, $bmp, $mood, $genre, $title){ //Sorting
     $sql = "SELECT * FROM base_information";
@@ -47,6 +46,34 @@ function sortBy($link, $bmp, $mood, $genre, $title){ //Sorting
     return $data;
 }
 
+function add_user($data, $conn){
+    $id = mysqli_real_escape_string($conn, $data['new_member']['id']); #protect data from taken away
+    $img = mysqli_real_escape_string($conn, $data['new_member']['img']);
+    $title = mysqli_real_escape_string($conn, $data['new_member']['title']);
+    $time = mysqli_real_escape_string($conn, $data['new_member']['time']);
+    $bpm = mysqli_real_escape_string($conn, $data['new_member']['bpm']);
+    $tags = mysqli_real_escape_string($conn, $data['new_member']['tags']);
+    $link = mysqli_real_escape_string($conn, $data['new_member']['link']);
+    $price = mysqli_real_escape_string($conn, $data['new_member']['price']);
+    $key = mysqli_real_escape_string($conn, $data['new_member']['key']);
+    $mood = mysqli_real_escape_string($conn, $data['new_member']['mood']);
+    $genre = mysqli_real_escape_string($conn, $data['new_member']['genre']);
+    $soundSrc = mysqli_real_escape_string($conn, $data['new_member']['soundSrc']);
+
+
+    $sql = "INSERT INTO `base_information` 
+            (`id`, `img`, `title`, `time`, `bpm`, `tags`, `link`, `price`, `key`, `mood`, `genre`, `soundSrc`) 
+            VALUES 
+            ('$id', '$img', '$title', '$time', '$bpm', '$tags', '$link', '$price', '$key', '$mood', '$genre', '$soundSrc')";
+
+    if (mysqli_query($conn, $sql)) {
+        return true;
+    } else {
+        echo "Ошибка: " . mysqli_error($conn);
+        return false;
+    }
+}
+
 $app->add(function ($request, $handler) { #Request access
     $response = $handler->handle($request);
     return $response
@@ -54,17 +81,6 @@ $app->add(function ($request, $handler) { #Request access
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
-
-
-// $app->get('/message_value', function (Request $request, Response $response, array $args) {
-//     $queryParams = $request->getQueryParams();
-//     if (isset($queryParams['message'])) {
-//         $response->getBody()->write($queryParams['message']);
-//     } else {
-//         $response->getBody()->write('No message provided');
-//     }
-//     return $response;
-// });
 
 $app->get('/database', function (Request $request, Response $response, array $args) {
     $link = mysqli_connect("localhost", "root", "", "INFO");
@@ -78,6 +94,61 @@ $app->get('/database', function (Request $request, Response $response, array $ar
     $sorted_data = sortBy($link, $bpm, $mood, $genre, $title);
 
     $response->getBody()->write(json_encode($sorted_data));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+// $app->add(function ($request, $handler) { #Request access
+//     $response = $handler->handle($request);
+//     return $response
+//             ->withHeader('Access-Control-Allow-Origin', '*')
+//             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+//             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+// });
+
+$app->post('/POST', function (Request $request, Response $response, $args) {
+    $uploadedFiles = $request->getUploadedFiles();
+    $uploadedFile = $uploadedFiles['file'];
+
+    if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+        $uploadPath = $request->getParsedBody()['path'];
+        $uploadedFileName = $uploadedFile->getClientFilename();
+        $uploadedFile->moveTo($uploadPath . $uploadedFileName);
+
+        return $response->withHeader('Content-Type', 'application/json');
+    } else {
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+});
+
+$app->post('/Database/add_user', function (Request $request, Response $response, $args) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    print_r($data);
+    $link = mysqli_connect("localhost", "root", "", "INFO");
+
+    add_user($data, $link);
+
+    mysqli_close($link);
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/Database/delete_user', function (Request $request, Response $response, $args) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    print_r($data);
+
+    $link = mysqli_connect("localhost", "root", "", "INFO");
+
+    $id = mysqli_real_escape_string($link, $data['id']);
+    $sql = "DELETE FROM `base_information` WHERE `id` = '{$id}'";
+
+    if (mysqli_query($link, $sql)) {
+        echo "Запись успешно удалена из базы данных";
+    } else {
+        echo "Ошибка при выполнении запроса: " . mysqli_error($link);
+    }
+
+    mysqli_close($link);
+
     return $response->withHeader('Content-Type', 'application/json');
 });
 
