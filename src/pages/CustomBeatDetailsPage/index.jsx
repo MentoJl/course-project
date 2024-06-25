1e729a3f49027c0cc9b5ba4600a1b172f177a136
djproblematic <djproblematicbusiness@gmail.com>
2024-06-25 18:33:51 +0300

    update

--- src/pages/CustomBeatDetailsPage/index.jsx
+++ src/pages/CustomBeatDetailsPage/index.jsx
@@ -1,11 +1,17 @@
 import { Button, Image } from 'antd'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
 import styles from './style.module.css'
 
 const CustomBeatDetailsPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const messageRef = useRef(null)
+  const sendMail = useRef(null)
+  const sendName = useRef(null)
+  const sendProject = useRef(null)
+  const sendPhone = useRef(null)
+  const sendBudget = useRef(null)
 
   const openModal = () => {
     setIsModalOpen(true)
@@ -15,6 +21,43 @@ const CustomBeatDetailsPage = () => {
   const closeModal = () => {
     setIsModalOpen(false)
     document.body.style.overflow = 'auto'
+  }
+
+  const handleSendEmail = () => {
+    const emailData = {
+      email: sendMail.current.value,
+      title: `CUSTOM BEAT`,
+      name: sendName.current.value,
+      message: messageRef.current.value,
+      projectName: sendProject.current.value,
+      phone: sendPhone.current.value,
+      budget: sendBudget.current.value,
+    }
+
+    if (
+      sendMail.current.value !== '' &&
+      sendProject.current.value !== '' &&
+      sendName.current.value !== '' &&
+      messageRef.current.value !== '' &&
+      sendProject.current.value !== '' &&
+      sendPhone.current.value !== '' &&
+      sendBudget.current.value !== ''
+    ) {
+      fetch('http://database/sendEmail.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(emailData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Success:', data)
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+        })
+    }
   }
 
   return (
@@ -72,20 +115,20 @@ const CustomBeatDetailsPage = () => {
           </div>
           <form className={styles.form}>
             <div className={styles.formRow}>
-              <input className={styles.inputField} type="text" placeholder="ARTIST NAME" />
-              <input className={styles.inputField} type="email" placeholder="E-MAIL" />
+              <input className={styles.inputField} ref={sendName} type="text" placeholder="ARTIST NAME" />
+              <input className={styles.inputField} ref={sendMail} type="email" placeholder="E-MAIL" />
             </div>
             <div className={styles.formRow}>
-              <input className={styles.inputField} type="text" placeholder="PROJECT NAME" />
+              <input className={styles.inputField} ref={sendProject} type="text" placeholder="PROJECT NAME" />
             </div>
             <div className={styles.formRow}>
-              <input className={styles.inputField} type="text" placeholder="PHONE NUMBER" />
-              <input className={styles.inputField} type="text" placeholder="BUDGET AMOUNT (USD)" />
+              <input className={styles.inputField} ref={sendPhone} type="text" placeholder="PHONE NUMBER" />
+              <input className={styles.inputField} ref={sendBudget} type="text" placeholder="BUDGET AMOUNT (USD)" />
             </div>
             <div className={styles.formRow}>
-              <textarea className={styles.textArea} placeholder="PROJECT DESCRIPTION"></textarea>
+              <textarea className={styles.textArea} ref={messageRef} placeholder="PROJECT DESCRIPTION"></textarea>
             </div>
-            <button className={styles.submitButton} type="submit">
+            <button className={styles.submitButton} onClick={handleSendEmail} type="submit">
               SEND INQUIRY
             </button>
           </form>
