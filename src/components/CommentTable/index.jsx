be9b668e07397d8745823144c00290b772c7bc62
djproblematic <djproblematicbusiness@gmail.com>
2024-06-26 16:24:49 +0300

    update

--- src/components/CommentTable/index.jsx
+++ src/components/CommentTable/index.jsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from "react";
-import styles from './style.module.css';
-import axios from "axios";
-import { message } from "antd";
+import { message } from 'antd'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import styles from './style.module.css'
 
 const CommentTable = ({ title = 'Beef' }) => {
-  const [commentList, setCommentList] = useState([]);
+  const [commentList, setCommentList] = useState([])
 
   const addComment = (title, comment) => {
     const newRow = {
       title: <span className={styles.commentTitle}>{title}</span>,
       comment: <span className={styles.commentText}>{comment}</span>,
-    };
-    setCommentList((prevCommentList) => [...prevCommentList, newRow]);
-  };
+    }
+    setCommentList((prevCommentList) => [...prevCommentList, newRow])
+  }
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const response = await axios.get(`http://database/takeAction?beat_name=${title}&action=comment`);
+        const response = await axios.get(`http://database/takeAction?beat_name=${title}&action=comment`)
+        console.log(response)
         response.data.forEach((data) => {
-          addComment(data.login, data.value);
-        });
+          addComment(data.login, data.value)
+        })
         message.success(`Success Load`)
       } catch (err) {
-        message.error(`Error to load comments: ${err}`);
+        message.error(`Error to load comments: ${err}`)
       }
-    };
+    }
 
-    fetchComments();
-  }, title);
+    fetchComments()
+  }, title)
 
   return (
     <div className={styles.commentTableContainer}>
       <table className={styles.commentTable}>
         {commentList.map((comment) => (
-        <tr className={styles.commentContainer}>
-          <td className={styles.commentTitleContainer}>
-            <span className={styles.commentTitle}>{comment.title}</span>
-          </td>
-          <td className={styles.commentTextContainer}>
-            <span className={styles.commentText}>{comment.comment}</span>
-          </td>
-        </tr>
+          <tr className={styles.commentContainer}>
+            <td className={styles.commentTitleContainer}>
+              <span className={styles.commentTitle}>{comment.title}</span>
+            </td>
+            <td className={styles.commentTextContainer}>
+              <span className={styles.commentText}>{comment.comment}</span>
+            </td>
+          </tr>
         ))}
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default CommentTable;
+export default CommentTable
